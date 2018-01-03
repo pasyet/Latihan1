b6f879a9826f6a02c33ea4e369a393c6c907a48b
pasyet <farapasyetnabila@gmail.com>
2018-01-03 14:13:45 +0700

    Latihan 1

---
+++ Lingkaran.java
@@ -0,0 +1,9 @@
+import java.lang.Math;
+
+public class Lingkaran {
+    public static void main(String[] args){
+        Integer jari2 = 15;
+        Double keliling = 2*jari2 * Math.PI;
+        System.out.println("Nilai PI "+Math.PI+", maka kelilingnya adalah "+keliling);
+    }
+}
---
+++ Persegi.java
@@ -0,0 +1,6 @@
+public class Persegi{
+    public static void main (String[] args){
+        java.lang.Double sisi = 50d;
+        System.out.println("Dengan sisi 50 cm maka luas persegi tersebut adalah " +(sisi*sisi/100)+ " m ");
+    }
+}
